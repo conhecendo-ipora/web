93a450bef2589a00eb5ec30043d045d0a3c1adfd
PedroRaphaelGomes <pedro.gomes@estudante.ifgoiano.edu.br>
2022-01-26 17:48:50 -0300

    Carrosel

--- scripts.js
+++ scripts.js
@@ -13,3 +13,31 @@ btn_menu.addEventListener("click", ()=>{
         menu_Out.classList.add("active");
     }
 })
+
+/* Carrosel */
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+var timg = document.getElementsByClassName("tamanho")[0].width;
+
+window.addEventListener("resize",function(){
+    timg = document.getElementsByClassName("tamanho")[0].width;     
+});
+
+
+let idx = 0;
+
+function carrosel(){
+    idx++;
+
+
+
+    if(idx > img.length-1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * timg}px)`;
+};
+
+setInterval(carrosel, 1800);
